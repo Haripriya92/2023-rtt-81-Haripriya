0a70bb1fa37bec9433343e884029a3231f4e071b
HARIPRIYA92 <haripriyamnair1992@gmail.com>
2023-11-01 15:20:27 -0500

    no message

---
+++ springboot/springboot/src/main/java/org/perscholas/springboot/database/entity/User.java
@@ -0,0 +1,16 @@
+package org.perscholas.springboot.database.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+}
